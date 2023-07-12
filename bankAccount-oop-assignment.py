b4bcfec74559b5b5bc0d0b4b802657c8b1595a20
Gabbe Grodin, MFA <gabbe.grodin@gmail.com>
2023-07-12 16:26:25 -0500

    debugged yield_interest method

--- bankAccount-oop-assignment.py
+++ bankAccount-oop-assignment.py
@@ -27,16 +27,16 @@ class BankAccount:
 
     def yield_interest(self):
         if self.balance > 0:
-            self.balance + self.balance * self.interest_rate
+            self.balance = self.balance + self.balance * self.interest_rate
             print(f"Account {self.acct_no} Interest rate generated increased of: ${self.balance * self.interest_rate}")
             return self
         else:
             print(f"no increase possible")
         # while current balance is > 0: current balance * interest rate
 account_000001 = BankAccount("000001",.0495,2.25)
-account_000002 = BankAccount("000002",.046,1500)
-account_000003 = BankAccount("000003",.03,5000)
+# account_000002 = BankAccount("000002",.046,1500)
+# account_000003 = BankAccount("000003",.03,5000)
 
 account_000001.deposit(10.0).deposit(10.0).deposit(10.0).withdrawal(55).yield_interest().display_account_info()
-account_000002.display_account_info().deposit(100.75).withdrawal(250).withdrawal(250).withdrawal(250).yield_interest().display_account_info()
-account_000003.display_account_info().deposit(4500.0).withdrawal(25.45).yield_interest().display_account_info()
+# account_000002.display_account_info().deposit(100.75).withdrawal(250).withdrawal(250).withdrawal(250).yield_interest().display_account_info()
+# account_000003.display_account_info().deposit(4500.0).withdrawal(25.45).yield_interest().display_account_info()
